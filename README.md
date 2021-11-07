dcc557f253eeff725243a84b36a7dc6139aff202
orsifrancesco <hello@orsi.me>
2021-11-07 14:22:53 +0000

    first test

---
+++ README.md
@@ -0,0 +1,33 @@
+# react-mini-alert-confirm
+
+[![NPM version](https://img.shields.io/npm/v/mini-alert-confirm.svg?style=flat)](https://www.npmjs.com/package/mini-alert-confirm)
+
+Simple and easy configurable alert/confirm dialog box for Javascript.
+
+![mini-alert-confirm](https://user-images.githubusercontent.com/6490641/83878015-5fc2e400-a733-11ea-93d6-b157c96afd46.gif "mini-alert-confirm")
+
+## Installation
+
+```bash
+$ npm i mini-alert-confirm
+```
+
+## Examples
+
+```js
+
+    import miniAlertConfirm from 'mini-alert-confirm';
+    // import 'mini-alert-confirm/index.css';       // <!-- CSS file (optional, if you want to customize the dialog box)
+
+    alert = function (text) { miniAlertConfirm(text); }
+    confirm = function (text, callback) { miniAlertConfirm(text, callback); }
+
+```
+
+## Demo
+
+[Just a example project where you can see mini-alert-confirm in action..](https://passwordonce.com)
+
+## License
+
+Licensed under MIT
---
+++ index.js
@@ -0,0 +1,57 @@
+import 'index.css';
+
+window.addEventListener('load', () => {
+
+    var newEl = document.createElement('div');
+    newEl.id = "mini-alert-confirm";
+    newEl.innerHTML = (`
+<div class="box">
+    <div class="ico"></div>
+    <div class="text"></div>
+    <div class="buttons-container">
+        <div class="button">Ok</div>
+    </div>
+    <div class="buttons-container buttons-container--confirm">
+        <div class="button button--v2">Cancel</div>
+        <div class="button button--ok">Ok</div>
+    </div>
+</div>
+`);
+    document.querySelector("body").appendChild(newEl);
+
+});
+
+const close = function () {
+    document.querySelector("html").classList.remove("show-mini-alert-confirm");
+    setTimeout(function () {
+        document.querySelector("html").classList.remove("show-mini-alert-confirm--confirm");
+    }, 500);
+}
+
+const miniAlertConfirm = function (text, callback) {
+
+    document.querySelector("html").classList.add("show-mini-alert-confirm");
+    document.querySelector("#mini-alert-confirm .text").innerHTML = text;
+
+    document.querySelector("#mini-alert-confirm .button").removeEventListener("click", close);
+    document.querySelector("#mini-alert-confirm .button").addEventListener("click", close);
+
+    if (callback) {
+        const confirmBind = function () { callback(); close(); }
+        document.querySelector("html").classList.add("show-mini-alert-confirm--confirm");
+        document.querySelector("#mini-alert-confirm .button--ok").removeEventListener("click", confirmBind);
+        document.querySelector("#mini-alert-confirm .button--ok").addEventListener("click", confirmBind);
+    }
+
+}
+
+export default miniAlertConfirm;
+
+/*
+
+import miniAlertConfirm from 'miniAlertConfirm';
+
+alert = function (text) { miniAlertConfirm(text); }
+confirm = function (text, callback) { miniAlertConfirm(text, callback); }
+
+*/
